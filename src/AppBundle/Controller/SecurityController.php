2ecb4eaa8f7ffe9b6fb72c8d5a94237ddfb5667e
pafik13 <re1ax_88@mail.ru>
2015-09-03 00:25:36 +0400

    Auth and Main page

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?PHP
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login_route")
+     */
+	public function loginAction(Request $request)
+	{
+		$authenticationUtils = $this->get('security.authentication_utils');
+
+		// get the login error if there is one
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		// last username entered by the user
+		$lastUsername = $authenticationUtils->getLastUsername();
+
+		return $this->render(
+			'/security/login.html.twig',
+			array(
+				// last username entered by the user
+				'last_username' => $lastUsername,
+				'error'         => $error,
+			)
+		);
+	}
+}
