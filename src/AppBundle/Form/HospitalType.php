1b59ee6a567e1c5fdfbf762bafd74a7cf4892d0c
dasha <irvill11@gmail.com>
2016-01-19 19:18:07 +0300

    hospital

---
+++ src/AppBundle/Form/HospitalType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HospitalType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('shortName', TextType::class, array('label' => 'Краткое наименование'))
+            ->add('fullName', TextType::class, array('label' => 'Полное наименование', 'attr' => array('pattern' => '[А-Я][а-я]+')))
+            ->add('territory', EntityType::class, array('class'=>'AppBundle:Territory','choice_label'=>'name','label' => 'Округ','placeholder'=>'Не указан', 'required' => false))
+            ->add('address', TextType::class, array('label'=>'Адрес', 'read_only'=>'read_only'))
+            ->add('map',ButtonType::class, array('label'=>'Карта'))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Hospital'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'appbundle_hospital';
+    }
+}
